877ca50ce0ea1b7ec8d6c8ec425521bad14316a8
AphiweMp <aphiwempeto@gmail.com>
2022-02-22 10:02:55 +0200

    fix error

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+//require('dotenv').config()
 
 const express = require('express')
 const app = express()
@@ -18,5 +18,5 @@ app.use('/subscribers', subscribersRouter)
 
 
 
-app.listen(3000, () => console.log('Server Started'))
+app.listen(process.env.PORT || 3000, () => console.log('Server Started'))
 
